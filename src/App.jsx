d3d75bfb207d4e96af31716ad7bc48ecfd5b7dc4
Sijan666 <666majharulislam@gmail.com>
2025-12-17 18:33:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import RootLayouts from './components/layouts/RootLayouts'
+import Home from './components/pages/Home'
+
+
+function App () {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<RootLayouts />}>
+          <Route index element={<Home/>} />
+          {/* <Route path="/about" element={<About/>} /> */}
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
+
+

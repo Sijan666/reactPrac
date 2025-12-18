0a322e05e877e14f36b43513f28bada63a68273d
Sijan666 <666majharulislam@gmail.com>
2025-12-18 20:51:59 +0600

    Toba apu final

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../layouts/Banner";
+import GetCerto from "../layouts/GetCerto";
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <GetCerto/>
     </>
   );
 };

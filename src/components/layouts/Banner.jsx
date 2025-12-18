c6a17af23fdc2ddfc7bd4152e6c063fd9b93d091
jannatul281205 <jannatulferdausharshe@gmail.com>
2025-12-18 16:40:03 +0600

    Banner Image added

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -2,34 +2,40 @@ import React from "react";
 import Container from "../Container";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import Image from "../Image";
 
 const Banner = () => {
   return (
-    <Container>
-      <div className="w-[45%]">
-        <h1 className="font-inter font-extrabold text-[60px] text-[#02033B] leading-[75px]">
-          Your mobile privacy is our mission
-        </h1>
-        <p className="mt-[52px] mb-[96px] leading-[30px] font-inter font-normal text-[20px] text-[#02033B]">
-          Think your phone has been hacked? Our trusted apps make it easy for
-          you to scan, detect and remove threats from your iPhone and Android
-          devices.
-        </p>
-        <div className="flex">
-          <Link to={"/"}>
-            <button className="flex items-center gap-x-[16px] py-[16px] px-[20px] hover:bg-[#FFC247] outline-2 outline-[#02033B] rounded-[42px] font-inter font-extrabold text-[19px] text-[#02033B] cursor-pointer duration-300 hover:outline-[#FFC247]">
-              Get Certo for iPhone{" "}
-              <FaArrowRightLong className="text-[#000000]" />
-            </button>
-          </Link>
-          <Link to={"/"}>
-            <button className="py-[16px] px-[20px] hover:bg-[#FFC247] outline-2 outline-[#02033B] rounded-[42px] ml-[48px] font-inter font-extrabold text-[19px] text-[#02033B] cursor-pointer duration-300 hover:outline-[#FFC247] hover:outline-2">
-              Get Certo for Android
-            </button>
-          </Link>
-        </div>
+    <>
+      {" "}
+      <div className="bg-[url(/src/assets/banner.png)] bg-cover bg-no-repeat bg-center py-[123px]">
+        <Container>
+          <div className="w-[45%]">
+            <h1 className="font-inter font-extrabold text-[60px] text-[#02033B] leading-[75px]">
+              Your mobile privacy is our mission
+            </h1>
+            <p className="mt-[52px] mb-[96px] leading-[30px] font-inter font-normal text-[20px] text-[#02033B]">
+              Think your phone has been hacked? Our trusted apps make it easy
+              for you to scan, detect and remove threats from your iPhone and
+              Android devices.
+            </p>
+            <div className="flex">
+              <Link to={"/"}>
+                <button className="flex items-center gap-x-[16px] py-[16px] px-[20px] hover:bg-[#FFC247] outline-2 outline-[#02033B] rounded-[42px] font-inter font-extrabold text-[19px] text-[#02033B] cursor-pointer duration-300 hover:outline-[#FFC247]">
+                  Get Certo for iPhone{" "}
+                  <FaArrowRightLong className="text-[#000000]" />
+                </button>
+              </Link>
+              <Link to={"/"}>
+                <button className="py-[16px] px-[20px] hover:bg-[#FFC247] outline-2 outline-[#02033B] rounded-[42px] ml-[48px] font-inter font-extrabold text-[19px] text-[#02033B] cursor-pointer duration-300 hover:outline-[#FFC247] hover:outline-2">
+                  Get Certo for Android
+                </button>
+              </Link>
+            </div>
+          </div>
+        </Container>
       </div>
-    </Container>
+    </>
   );
 };
 

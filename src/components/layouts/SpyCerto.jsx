31a3f209e96418fdcf803b6aa77528f356a2e381
Sijan666 <666majharulislam@gmail.com>
2025-12-19 19:55:46 +0600

    refactor SpyCerto component layout and styles for improved consistency

--- src/components/layouts/SpyCerto.jsx
+++ src/components/layouts/SpyCerto.jsx
@@ -6,19 +6,25 @@ import { Link } from "react-router-dom"
 
 const SpyCerto = () => {
   return (
-  <div className="bg-gradient-to-b from-[#2925CC] to-[#4B48E5] p-20">
-   <Container>
-    <div className="text-center">
-         <h1 className="text-[34px] font-extrabold font-inter leading-[45px] text-white">Is someone spying on your phone?</h1>
-     <p className="text-white text-[18px] font-inter py-[50px]">Find out with Certo</p>
-    </div>
-    <div className="text-center px-[385px] py-7">
-        <Flex className={"gap-x-[24px]"}> 
-            <Link to={"/iphone"}> <Button className={"flex items-center gap-x-4 text-[#02033B] text-[19px] font-extrabold font-inter hover:text-white hover:border-white hover:border-2"}>Get Certo for iPhone<FaArrowRightLong /></Button> </Link>
-      <Link to={"/android"}> <Button className={"text-[#02033B] text-[19px] font-extrabold font-inter hover:text-white hover:border-white hover:border-2"}>Get Certo for Android</Button> </Link>
-        </Flex>
-    </div>
-   </Container>
+  <div className="bg-linear-to-b from-[#2925CC] to-[#4B48E5] p-20">
+    <Container>
+      <div className="text-center">
+          <h1 className="text-[34px] font-extrabold font-inter leading-[45px] text-white">Is someone spying on your phone?</h1>
+          <p className="text-white text-[18px] font-inter py-[50px]">Find out with Certo</p>
+      </div>
+      <div className="text-center px-[385px] py-7">
+          <Flex className={"gap-x-6"}> 
+              <Link to={"/iphone"}>
+                <Button className={"flex items-center gap-x-4 text-common text-[19px] font-extrabold font-inter hover:text-white hover:border-white hover:border-2"}>Get Certo for iPhone
+                  <FaArrowRightLong />
+                </Button> 
+              </Link>
+              <Link to={"/android"}>
+                <Button className={"text-common text-[19px] font-extrabold font-inter hover:text-white hover:border-white hover:border-2"}>Get Certo for Android</Button>
+              </Link>
+          </Flex>
+      </div>
+    </Container>
   </div>
   )
 }

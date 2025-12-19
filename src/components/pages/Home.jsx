eb1acd396181aebb533a67e1f5e4adec61b75037
Sijan666 <666majharulislam@gmail.com>
2025-12-19 22:04:48 +0600

    Nipa apu slick slider

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Images from './Images';
 
+
 const Home = () => {
   const sliderRef = useRef(null);
 

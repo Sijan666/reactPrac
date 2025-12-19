f67e8435e2387f48c83a60a7c323b4e87a75b803
Nipa Nusrat <nusratnipa028@gmail.com>
2025-12-19 17:37:06 +0600

    Add react-slick and slick-carousel for image slider; update Home component to include slider functionality and new Images page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import RootLayouts from './components/layouts/RootLayouts'
 import Home from './components/pages/Home'
+import React from 'react'
+import Images from './components/pages/Images'
 
-
-function App () {
+function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<RootLayouts />}>
-          <Route index element={<Home/>} />
-          {/* <Route path="/about" element={<About/>} /> */}
+          <Route index element={<Home />} />
+          <Route path="/images" element={<Images />} />
+          {/*<Route path="/about" element={<About/>} /> */}
         </Route>
       </Routes>
     </>
--- src/components/pages/Images.jsx
+++ src/components/pages/Images.jsx
@@ -7,3 +7,4 @@ const Images = ({ src }) => {
 }
 
 export default Images;
+
